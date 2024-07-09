fc80e8439d5c542f916685bc64cc23f4b109a1a7
Hugo de Freitas <hugo.encaminhados@gmail.com>
2024-07-09 11:33:38 -0300

    Primeiro commit

---
+++ index.js
@@ -0,0 +1,33 @@
+function organizeNumbers() {
+    $('#organizeBtn').addClass('clicked');
+    $('#values').css('margin-top', '-2px');
+    setTimeout(() => {
+        $('#organizeBtn').removeClass('clicked');
+        $('#values').css('margin-top', '0px');
+    }, 100);
+
+    const redMessage = {
+        color: 'red',
+        fontWeight: '700'
+    };
+
+    const input = $('#numbers').val().split(" ");
+    const answerSpace = $('#values');
+    let aux;
+
+    // for (let i = 0; i < input.length; i++){
+    //     console.log(typeof(input[i]))
+    // }
+
+    for (let i = 0; i < input.length; i++) {
+        for (let j = i + 1; j < input.length; j++) {
+            if (input[j] < input[i]) {
+                aux = input[j];
+                input[j] = input[i];
+                input[i] = aux;
+            }
+        }
+    }
+
+    // input == '' ? answerSpace.text("Nenhum valor inserido").css(redMessage) : answerSpace.text(input.join(", "));
+}
